ac26c03bf9db643847a4ec439c81484066599c58
Chen Youxue <596798600@qq.com>
2014-05-06 13:53:36 -0400

    Stater Project
    Hello World

---
+++ Day2Code/Day2Code/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Day2Code
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 Chen YX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
